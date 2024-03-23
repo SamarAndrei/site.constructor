4f9ec9fbaf15b5816cffb8a0bb8d4832f48845d5
Danil <toxen2005@mail.ru>
2024-03-23 15:46:15 +0500

    'dwdw'

--- BackEnd/site_constructor/site_constructor/urls.py
+++ BackEnd/site_constructor/site_constructor/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', page)
 ]
