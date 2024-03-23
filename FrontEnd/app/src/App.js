d238f2ec11a3427d1d0583144a32982a04b3ce62
Stepa <stepan.09myromec@gmail.com>
2024-03-23 20:34:19 +0500

    rabota

--- FrontEnd/app/src/App.js
+++ FrontEnd/app/src/App.js
@@ -1,11 +1,6 @@
 import BasicButtons from './components/UI/Buttons/Button';
-<<<<<<< HEAD
-import React from 'react';
-
-=======
 import TypographyTheme from './components/UI/Text/Text';
 import BasicTextFields from './components/UI/Input/Input';
->>>>>>> 6725e1d3cae44051b3aebfe15fd24f8f5f80aed7
 
 function App() {
   return (
