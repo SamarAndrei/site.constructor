6725e1d3cae44051b3aebfe15fd24f8f5f80aed7
n530autique <darkness2241@list.ru>
2024-03-23 20:27:24 +0500

    text

--- FrontEnd/app/src/App.js
+++ FrontEnd/app/src/App.js
@@ -1,10 +1,14 @@
 import BasicButtons from './components/UI/Buttons/Button';
-
+import TypographyTheme from './components/UI/Text/Text';
+import BasicTextFields from './components/UI/Input/Input';
 
 function App() {
   return (
     <div className="App">
       <BasicButtons/>   
+      <TypographyTheme/>
+      <BasicTextFields/>
+
     </div>
   );
 }
