27e0a2701df3d5b39c6de5694fed03eec72a7ccf
Andrei Samar <istore103@icloud.com>
2024-03-24 19:44:23 +0500

    'users'

--- BackEnd/site_constructor/users/models.py
+++ BackEnd/site_constructor/users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Users(models.Model):
+    name = models.CharField(max_length = 100, verbose_name = 'Имя пользователя')
+    email = models.EmailField()
+    password = models.CharField(max_length = 25, widget = models.PasswordInput)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    photo = models.ImageField(upload_to='photo/%Y/%/%d/', blanck =True)
