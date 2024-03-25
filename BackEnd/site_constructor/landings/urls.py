d2e6b55efc95c511ffa64a74491a099819ac6e80
Andrei Samar <istore103@icloud.com>
2024-03-25 15:40:06 +0500

    debug

--- BackEnd/site_constructor/landings/urls.py
+++ BackEnd/site_constructor/landings/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 urlpatterns = [
    path('landing_add/', landing_add),
-   path('landing_delet', landing_delete),
+   path('landing_delete/', landing_delete),
    path('landing_get/', landing_get),
    path('landing_get_all/', landing_get_all),
    path('landing_update/', landing_update)
