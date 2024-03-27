02b83105b3306d2bbd7df7cad41f2fab4f163a0f
Arsen <djuravlik323@gmail.com>
2024-03-27 20:28:39 +0500

    test-slider

--- FrontEnd/app/src/components/UI/Slider/TestSlider.jsx
+++ FrontEnd/app/src/components/UI/Slider/TestSlider.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
-import { Button, Stack } from '@mui/material';
-
+import { Button, Stack} from '@mui/material';
+import './TestSlider.css';
 const TestSlider = () => {
 	return (
 		<div>
@@ -10,53 +10,35 @@ const TestSlider = () => {
         naturalSlideHeight={10}
         totalSlides={3}
       >
-
-        <Slider style={{
-                width: '50%', 
-                margin: '0 auto',
-                background: "indigo",
-                color: "white"
-                      }}>
-            
-          <Slide index={0} style={{textAlign: 'center'}}>
-          <div style={{ fontSize: '24px', fontFamily: 'Arial Black, sans-serif', textTransform: 'lowercase' }}>
+        <Slider className='slider'>     
+          <Slide index={0} className='slide'>
+          <div >
           I am the first Slide.
           </div>
           </Slide>
 
-          <Slide index={1} style={{textAlign: 'center'}}>
-          <div style={{ fontSize: '24px', fontFamily: 'Arial Black, sans-serif', textTransform: 'lowercase' }}>
+          <Slide index={1} className='slide'>
+          <div>
           I am the second Slide.
           </div>
           </Slide>
 
-          <Slide index={2} style={{textAlign: 'center'}}>
-          <div style={{ fontSize: '24px', fontFamily: 'Arial Black, sans-serif', textTransform: 'lowercase' }}>
+          <Slide index={2} className='slide'>
+          <div>
           I am the third Slide.
           </div>
           </Slide>
-
         </Slider>
 
         <div className="slider-buttons">
-            <Stack direction = "row" spacing = {2} justifyContent="center">
-              <ButtonBack style={{
-                          display: 'flex',
-                          justifyContent: 'center',
-                          alignContent: 'center',
-                          margin: '5px',
-                          border: '0px',
-                }}>
-                <Button variant="contained" color="secondary" size = "large" >Back</Button>
+            <Stack direction = "row" spacing = {2} justifyContent="center" alignItems="center" className='stack-style'
+              >
+              <ButtonBack style = {{border: '0px'}}>
+                <Button variant= "contained" color= "secondary" size = 'large' >Back</Button>
               </ButtonBack>
 
-              <ButtonNext style={{
-                          display: 'flex',
-                          justifyContent: 'center',
-                          alignContent: 'center',
-                          margin: '5px',
-                          border: '0px',}}>
-                <Button variant="contained" color="secondary" size = "large">Next</Button>
+              <ButtonNext style = {{border: '0px'}}>
+                <Button variant= "contained" color= "secondary" size= "large" >Next</Button>
               </ButtonNext>
             </Stack>
           </div>
