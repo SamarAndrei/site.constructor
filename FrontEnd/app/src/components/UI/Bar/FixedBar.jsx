d26a8f0814bb4ea37d5f2a607629b882df3db6e6
Arsen <djuravlik323@gmail.com>
2024-03-31 19:28:25 +0500

    header

--- FrontEnd/app/src/components/UI/Bar/FixedBar.jsx
+++ FrontEnd/app/src/components/UI/Bar/FixedBar.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
-import './FixedBar.css'; // Импортируем файл стилей
+import './FixedBar.css'; 
 import { Button} from '@mui/material';
+import logo from '../../../assets/images/logosite.png';
 const FixedBar = () => {
   return (
     <div className="fixed-bar">
-      <span>Тестовая панель. Маринованные перчики</span>
-      <Button variant = "contained" style={{ backgroundColor: '#000000', color: '#ffffff', fontFamily: 'Franklin Gothic Medium, Arial Narrow, Arial, sans-serif', width: '100px', height: '30px'}}>Кнопка</Button>
-      <img className = "logo" src = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Berserk_Logo.svg/2560px-Berserk_Logo.svg.png" alt=''></img>
+      <img className="logo" src={logo} alt="Логотип сайта" />
+      <span className="text">Сайт-конструктор "botat"</span>
+      <Button className="button" variant="text" style = {{
+        marginLeft: 'auto',
+        color: '#ffffff', 
+        fontFamily: 'Lucida Sans Unicode', 
+        width: '50px', 
+        height: '30px', 
+        marginRight: '20px', 
+      }}>
+        Войти
+      </Button>
     </div>
   );
 }
+
 
 export default FixedBar;
