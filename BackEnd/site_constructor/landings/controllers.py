73aee30a85a23c037ab6b31c9462b99b155c0e57
Danil <toxen2005@mail.ru>
2024-03-31 18:50:12 +0500

    'handles'

--- BackEnd/site_constructor/landings/controllers.py
+++ BackEnd/site_constructor/landings/controllers.py
@@ -8,61 +8,50 @@ def landing_add(request):
     if request.method == 'POST':
         name = request.POST.get('name')
         address = request.POST.get('address')
-        n = Landing(land_name=name, land_address=address)
-        n.save()
-        return JsonResponse({'message': 'Landing added successfully'})
-    return HttpResponseNotAllowed(['POST'])
+
+        landing_service = LandingService()
+        success = landing_service.add_landing(name, address)
+
+        return JsonResponse({'success': success})
 
 
-def landing_delete(request, land_id):
+def landing_delete(request, landing_id):
     if request.method == 'DELETE':
-        n = get_object_or_404(Landing, pk=land_id)
-        n.delete()
-        return HttpResponse('Landing deleted successfully')
-    else:
-        return HttpResponseNotAllowed(['DELETE'])
+        landing_service = LandingService()
+        success = landing_service.delete_landing(landing_id)
+        return JsonResponse({'success': success})
 
 
-def landing_update(request, land_id):
+def landing_update(request, landing_id):
     if request.method == 'PUT':
-        n = get_object_or_404(Landing, pk=land_id)
-        land_name_new = request.POST.get('land_name', '')
-        land_address_new = request.POST.get('land_address', '')
+        name_new = request.POST.get('name')
+        address_new = request.POST.get('address')
 
-        if land_name_new != "":
-            n.land_name = land_name_new
-            n.save()
-        if land_address_new != "":
-            n.land_address = land_address_new
-            n.save()
+        landing_service = LandingService()
+        success = landing_service.update_landing(landing_id, name_new, address_new)
 
-        return JsonResponse({'message': 'Landing updated successfully'})
-    else:
-        return HttpResponseNotAllowed(['PUT'])
+        return JsonResponse({'success': success})
 
 
 def landing_get_all(request):
     if request.method == 'GET':
-        landings = Landing.objects.all()
-        landing_list = [{'land_name': landing.land_name, 'land_address': landing.land_address} for landing in landings]
-        return JsonResponse(landing_list, safe=False)
-    else:
-        return HttpResponseNotAllowed(['GET'])
+        landing_service = LandingService()
+        success = landing_service.get_all_landing()
+        return JsonResponse({'success': success})
 
 
-def landing_get(request, land_id):
+def landing_get(request, landing_id):
     if request.method == 'GET':
-        n = get_object_or_404(Landing, pk=land_id)
-        return render(request, 'landing.html', {'landing': n})
-    else:
-        return HttpResponseNotAllowed(['GET'])
+        landing_service = LandingService()
+        success = landing_service.get_landing(landing_id)
+        return JsonResponse({'success': success})
 
 
 def user_add(request):
     if request.method == 'POST':
-        user_name = request.POST.get('user_name', '')
-        email = request.POST.get('email', '')
-        password = request.POST.get('password', '')
+        user_name = request.POST.get('user_name')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
 
         user_service = LandingService()
         success = user_service.add_user(user_name, email, password)
@@ -71,9 +60,9 @@ def user_add(request):
 
 def user_update(request, user_id):
     if request.method == 'PUT':
-        name_new = request.POST.get('name', '')
-        email_new = request.POST.get('email', '')
-        password_new = request.POST.get('password', '')
+        name_new = request.POST.get('name')
+        email_new = request.POST.get('email')
+        password_new = request.POST.get('password')
 
         user_service = LandingService()
         success = user_service.update_user(user_id, name_new, email_new, password_new)
--- BackEnd/site_constructor/landings/urls.py
+++ BackEnd/site_constructor/landings/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 
 urlpatterns = [
    path('', landing_add),
-   path('<int:land_id/>', landing_delete),
-   path('<int:land_id/>', landing_get),
+   path('<int:land_id>/', landing_delete),
+   path('<int:land_id>/', landing_get),
    path('', landing_get_all),
-   path('<int:land_id/>', landing_update),
-   path('users/<int:user_id/>', user_update),
+   path('<int:land_id>/', landing_update),
+   path('users/<int:user_id>/', user_update),
    path('users/', user_add),
-   path('users/<int:user_id/>', user_delete),
+   path('users/<int:user_id>/', user_delete),
 ]
