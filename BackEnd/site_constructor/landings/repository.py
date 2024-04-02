753d0a5ab6c8322d40f40019214fada8d398f349
Andrei Samar <istore103@icloud.com>
2024-04-02 23:56:11 +0500

    fix

--- BackEnd/site_constructor/landings/repository.py
+++ BackEnd/site_constructor/landings/repository.py
@@ -10,41 +10,29 @@ class LandingRepository:
         return True
 
     def _delete_landing(self, landing_id):
-        try:
-            landing = Landing.objects.get(id=landing_id)
-            landing.delete()
-            return True
-        except Landing.DoesNotExist:
-            return False
+        landing = Landing.objects.get(id=landing_id)
+        landing.delete()
+        return True
 
     def _update_landing(self, landing_id, name_new, address_new):
-        try:
-            landing = Landing.objects.get(id=landing_id)
-            if name_new != "":
-                landing.name = name_new
-                landing.save()
+        landing = Landing.objects.get(id=landing_id)
+        if name_new != "":
+            landing.name = name_new
+            landing.save()
 
-            if address_new != "":
-                landing.email = address_new
-                landing.save()
-            return True
-        except Landing.DoesNotExist:
-            return False
+        if address_new != "":
+            landing.email = address_new
+            landing.save()
+        return True
 
     def _get_landing(self, landing_id):
-        try:
-            landing = Landing.objects.get(id=landing_id)
-            return JsonResponse({'name': landing.name, 'address': landing.address})
-        except Landing.DoesNotExist:
-            return False
+        landing = Landing.objects.get(id=landing_id)
+        return JsonResponse({'name': landing.name, 'address': landing.address})
 
     def _get_all_landing(self):
-        try:
-            landings = Landing.objects.all()
-            landing_list = [{'name': landing.name, 'address': landing.address} for landing in landings]
-            return JsonResponse({'landing_list': landing_list})
-        except Landing.DoesNotExist:
-            return False
+        landings = Landing.objects.all()
+        landing_list = [{'name': landing.name, 'address': landing.address} for landing in landings]
+        return JsonResponse({'landing_list': landing_list})
 
 
     def _add_user(self, user_name, email, password):
