fa9cb73b7f31c7f6d70b059ff81ad3bfe8f1495d
Andrei Samar <istore103@icloud.com>
2024-05-22 14:46:57 +0500

    fixreg

--- FrontEnd/app/src/components/UI/Card/Card.jsx
+++ FrontEnd/app/src/components/UI/Card/Card.jsx
@@ -8,6 +8,10 @@ import { redirect } from 'react-router-dom';
 
 const Card = ({ login, showInputs = true, showButton = true, showLink = true, children }) => {
 
+    const [valueName, setValueName] = useState('');
+    const [valueEmail, setValueEmail] = useState('');
+    const [valuePass, setValuePass] = useState('');
+
     const handleClickReg = () => {
         axios.post('http://127.0.0.1:8000/users/register', {
             name: `${document.getElementsByName('Username').values}`,
@@ -26,13 +30,13 @@ const Card = ({ login, showInputs = true, showButton = true, showLink = true, ch
     return (
         <div className='container'>
             <img className='logoLogin' src={logo} alt='etagiki' />
+            
+            {showInputs && !login && <input value={valueName} onChange={e => setValueName(e.target.value)} placeholder='Username' name='Username' />}
+            {showInputs && <input value={valueEmail} onChange={e => setValueEmail(e.target.value)} placeholder='Email' name='Email' />}
+            {showInputs && <input value={valuePass} onChange={e => setValuePass(e.target.value)} placeholder='Password' name='Password' />}
 
-            {showInputs && !login && <Input placeholder='Username' name='Username' />}
-            {showInputs && <Input placeholder='Email' name='Email' />}
-            {showInputs && <Input placeholder='Password' name='Password' />}
-
-            {showButton && !login && <MyButton onClick={handleClickReg}>Зарегистрироваться</MyButton>}
-            {showButton && login && <MyButton onClick={handleClickLogin}>Войти</MyButton>}
+            {showButton && !login && <MyButton type='submit' disabled={valueName && valueEmail && valuePass ? false : true} onClick={handleClickReg}>Зарегистрироваться</MyButton>}
+            {showButton && login && <MyButton type='submit' disabled={valueEmail && valuePass ? false : true} onClick={handleClickLogin}>Войти</MyButton>}
 
             
 
